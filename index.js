263aad5b601c6f74cc3e17cd4e1e88e9449aa731
turkmenmuhammedeymen-dev <turkmenmuhammedeymen@gmail.com>
2026-08-14 13:37:42 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const ParseServer = require('parse-server').ParseServer;
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
 
 const api = new ParseServer({
   databaseURI: process.env.DATABASE_URI,
