98856346b341b891fe192e51b9db7785f74e606f
turkmenmuhammedeymen-dev <turkmenmuhammedeymen@gmail.com>
2026-08-15 22:57:18 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const ParseServer = require('parse-server').ParseServer;
-const cors = require('cors');
 
 const app = express();
-app.use(cors()); // Tüm isteklere izin ver
 
 try {
   const api = new ParseServer({
